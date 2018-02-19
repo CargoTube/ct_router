1e07b2d8b30aaba15ff8ebac5baaee0cd33294f9
bwegh <baswegh@gmx.de>
2018-02-19 20:47:24 +0100

    fix calling unexisting code

--- src/ct_router_app.erl
+++ src/ct_router_app.erl
@@ -6,7 +6,6 @@
          stop/1]).
 
 start(_StartType, _StartArgs) ->
-    ct_router_utils:init(),
     ct_router_sup:start_link().
 
 stop(_State) ->
--- src/ctr_utils.erl
+++ src/ctr_utils.erl
@@ -5,8 +5,5 @@
          gen_global_id/0
         ]).
 
-init() ->
-    ok.
-
 gen_global_id() ->
     rand:uniform(9007199254740993) - 1.
