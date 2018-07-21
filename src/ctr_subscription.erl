292fb5034167fe6e5c4409a820f7357b7252fc67
bwegh <baswegh@gmx.de>
2018-07-21 12:19:55 +0200

    add function to get the realm from subscription

--- src/ctr_subscription.erl
+++ src/ctr_subscription.erl
@@ -2,9 +2,14 @@
 
 -include("ct_router.hrl").
 
--export([to_map/1]).
+-export([to_map/1,
+         get_uri/1
+        ]).
 
 
 to_map(#ctr_subscription{id = Id, created = Created, uri = Uri,
                          match = Match }) ->
     #{id => Id, created => iso8601:format(Created), match => Match, uri => Uri}.
+
+get_uri(#ctr_subscription{uri = Uri}) ->
+    Uri.
