3133b980ada2f8559b1de97033cfaf40b904a477
bwegh <baswegh@gmx.de>
2018-08-15 11:47:58 +0200

    remove unused session record

--- include/ct_router.hrl
+++ include/ct_router.hrl
@@ -4,20 +4,6 @@
                     authmapping = []
                    }).
 
--record(ctr_session, {
-          id = undefined,
-          realm = undefined,
-          details = #{},
-          authid = undefined,
-          authrole = undefined,
-          authprovider = undefined,
-          authmethod = undefined,
-          authenticated = false,
-          subscriptions = [],
-          registrations = [],
-          peer_at_gate = undefined
-         }).
-
 -record(ctr_subscription, {
           id = undefined,
           realm = undefined,
